82ed3baeafc929924137618e9b90fc2a5aeabc19
ODBLK <1961069825dg@gmail.com>
2025-07-22 14:41:16 +0800

    Create Minify & Iconify Portal Mods.meta.js

---
+++ Minify & Iconify Portal Mods.meta.js
@@ -0,0 +1,11 @@
+// ==UserScript==
+// @name         Minify & Iconify Portal Mods
+// @namespace    https://github.com/ODBLK/IITC-plugin-Minify-Iconify-Portal-Mods
+// @version      20250722.1432
+// @description  Click mod bar to toggle between mod icon and abbreviation view. State is remembered.
+// @author       ODBLK
+// @match        https://intel.ingress.com/*
+// @grant        none
+// @downloadURL  https://raw.githubusercontent.com/ODBLK/IITC-plugin-Minify-Iconify-Portal-Mods/main/minify-iconify-mods.user.js
+// @updateURL    https://raw.githubusercontent.com/ODBLK/IITC-plugin-Minify-Iconify-Portal-Mods/main/minify-iconify-mods.meta.js
+// ==/UserScript==
